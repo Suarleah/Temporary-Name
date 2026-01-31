9c801ab9269482150f436adf64530cc4512230d0
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2026-01-31 08:27:28 -0600

    slight edits to the chairbrain to set up for logic

---
+++ Assets/Scripts/Ingame/PartyGoerBrain.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class PartyGoerBrain : MonoBehaviour
+{
+    public enum myMood {happy, neutral, sad};
+
+    public enum want {talk_with_someone, drink_with_someone, eat_with_someone, stay_sober, im_full, dont_want_to_talk, be_alone, dont_want_noise, sit_with_someone_with_mood_happy, sit_with_someone_with_mood_neutral, sit_with_someone_with_mood_sad, sit_with_someone_with_style_fancy } //styles are placeholders, styles are just for fun
+   
+    public bool satisfied; //visual cue + used for logic
+
+    public bool drinking; // visual cue
+
+    public bool eating; // visual cue
+
+    public bool talking; // visual cue
+
+    public bool too_noisy; // visual cue
+
+    public bool not_alone; // visual cue
+
+    //public PartyGoerBrain[] myNeighbors;
+    public ChairBrain currentChair;
+
+    public List<want> Wants;
+
+   
+    
+}
